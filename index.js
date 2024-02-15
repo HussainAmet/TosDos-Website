bc72b67ffdda877e66ad91ec30dcdb041adeb7ab
Hussain Amet <hussainamet@gmail.com>
2024-02-15 12:22:31 +0530

    commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect(process.env.MONGODB_SRV_STRING);
 
