47f1b87950bbf7295a77d3e6a5424520ba50c977
Hussain Amet <associationfunds53@gmail.com>
2024-11-14 20:15:11 +0530

    changes

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import 'esm';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.use(express.static("public"));
 
 
