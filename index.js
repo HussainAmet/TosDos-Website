51f1ac36806e43c2609316572cabad3a37e00ec7
Hussain Amet <associationfunds53@gmail.com>
2024-11-14 20:34:26 +0530

    commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
-// app.set('views', '/views');
+app.set('views', 'views');
 
 app.use(express.static("public"));
 
