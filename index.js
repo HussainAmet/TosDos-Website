0f70c2d26fcbe4134526a0ec93446d74e954cfdb
Hussain Amet <associationfunds53@gmail.com>
2024-11-14 21:06:20 +0530

    changes

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
